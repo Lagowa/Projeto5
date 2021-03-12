692fb43ed7e95758a24620cba5cad0815898c5d9
Lagowa <herbertlgw7@gmail.com>
2021-03-11 21:31:27 -0300

    aula2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,10 +29,15 @@ const routes: Routes = [
     path: 'about',
     loadChildren: () => import('./pages/about/about.module').then( m => m.AboutPageModule)
   },
+   {
+    path: 'view/:id',
+    loadChildren: () => import('./pages/view/view.module').then( m => m.ViewPageModule)
+   },
   {
     path: '**',
     loadChildren: () => import('./pages/e404/e404.module').then( m => m.E404PageModule)
-  }
+  },
+ 
 ];
 
 @NgModule({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCcELd9rTS6RdJoVz5dEEDGEaGZZfXDb6o",
+    authDomain: "munecos-16e7a.firebaseapp.com",
+    projectId: "munecos-16e7a",
+    storageBucket: "munecos-16e7a.appspot.com",
+    messagingSenderId: "276599014428",
+    appId: "1:276599014428:web:f35eff68bd3cdedbb40bd9"
+  }
 };
 
 /*
